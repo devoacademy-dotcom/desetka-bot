3dc6abdad4f673493f4c01a4fe33eb85a3bc97b0
devoacademy-dotcom <devoacademy@gmail.com>
2026-05-25 00:36:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const { Telegraf, Markup } = require('telegraf');
 const { createClient } = require('@supabase/supabase-js');
 
-const BOT_TOKEN = '8989739941:AAERbGHiWYKiBrnzU4s64sRbUmsX5Cz1gu8';
+const BOT_TOKEN = '8989739941:AAFYe1qHGsV-PTyaev3rix8ydvX9leAQ3oc';
 const SUPABASE_URL = 'https://atzcqmykrvvjvtgkxuxy.supabase.co';
 const SUPABASE_SERVICE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0emNxbXlrcnZ2anZ0Z2t4dXh5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3OTYwNDUwOCwiZXhwIjoyMDk1MTgwNTA4fQ.nGKWDkUGdeA1kjqhAyqdFMVfg29IvNMK6wukiTxUlaU';
 const MINI_APP_URL = 'https://strong-sopapillas-be36e8.netlify.app';
